b89f39ff560163d57414208cfdf62b49c81651d1
intinalambrico <web@internetinalambrico.com.co>
2022-07-11 11:25:24 -0500

    error 3

--- config/settings.php
+++ config/settings.php
@@ -10,8 +10,5 @@ return [
         "logErrorDetails"=>true,
         "determineRouteBeforeAppMiddleWare"=>true
     ],
-    "logger"	=> [
-    	"name" => 'hannillog',
-    	"path"	=> __DIR__."/logs/".$logDate->format('Y-m-d H-mm-ss')."app.log",
-    ],
+    
 ];
