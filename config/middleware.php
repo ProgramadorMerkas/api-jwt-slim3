c94ac53e41c6d3155c12d02943ef99e8be93ea7d
intinalambrico <web@internetinalambrico.com.co>
2022-09-17 15:28:53 -0500

    external token error13

--- config/middleware.php
+++ config/middleware.php
@@ -10,7 +10,7 @@ $logger->pushHandler($rotating);
 return function ($app)
 {
     $app->add(new Tuupola\Middleware\JwtAuthentication([
-        "ignore"=>["/auth/login" , "/external/searchCell" ,"/eternal/searchMail"],
+        "ignore"=>["/auth/login" , "/external/searchCell" ,"/external/searchMail"],
         "secret"=> JWT_SECRET_KEY,
         "logger"=> $logger,
         "error"=>function ($response,$arguments)
